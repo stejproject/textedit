7682af2519420dc133868615768c7ac3470be2eb
stejproject <belajarbroedu@gmail.com>
2022-10-14 02:41:01 +0700

    update copas

--- copy.js
+++ copy.js
@@ -1,7 +1,9 @@
-//Pass the id of the <input> element to be copied as a parameter to the copy()
-let copy = (textId) => {
-  //Selects the text in the <input> elemet
-  document.getElementById(textId).select();
-  //Copies the selected text to clipboard
-  document.execCommand("copy");
-};
+// Copy and paste in javascripts
+function paste() {
+  let pasteText = document.querySelector("#code-editor");
+  pasteText.focus();
+  document.execCommand("paste");
+  console.log(pasteText.textContent);
+}
+
+document.querySelector("#paste").addEventListener("click", paste);
